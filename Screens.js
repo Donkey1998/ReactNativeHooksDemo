f301243b82663a9517d05463133642a37d5cbb3a
Tony.li <tony.li@gaiaworks.cn>
2020-03-06 10:43:01 +0800

    添加react-navigation导航路由逻辑,添加Screens

---
+++ Screens.js
@@ -0,0 +1,7 @@
+import HooksDemo from './js/Page/HooksDemo';
+import HooksDemo2 from './js/Page/HooksDemo2'
+module.exports = {
+    HooksDemo: { screen: HooksDemo },
+    HooksDemo2: { screen: HooksDemo2 },
+};
+
